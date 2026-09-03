879d792ac365fa901a882d1d7266387da591e3f4
al3bdzo <abdalrhmanmohammed307@gmail.com>
2026-09-03 13:47:16 +0300

    database and registering a user!

--- handler_user.go
+++ handler_user.go
@@ -2,16 +2,57 @@ package main
 
 import (
 	"fmt"
+	"time"
+	"context"
+	"github.com/al3bdzo/Gator/internal/database"
+	"github.com/google/uuid"
 )
 
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) == 0 {
 		return fmt.Errorf("Usage: %s <name>", cmd.name)
 	}
-	err := s.cfg.SetUser(cmd.args[0])
+	name := cmd.args[0]
+
+	user, err := s.db.GetUser(context.Background(), name)
+	if err != nil {
+		return fmt.Errorf("Error getting user: %v", err)
+	}
+
+	err = s.cfg.SetUser(name)
 	if err != nil {
 		return fmt.Errorf("couldn't set the current user: %w", err)
 	}
+
 	fmt.Println("Login Successful!")
+	fmt.Printf("user data: %v\n", user)
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("Usage: %s <name>", cmd.name)
+	}
+
+	name := cmd.args[0]
+	now := time.Now()
+	userParams := database.CreateUserParams{
+		ID: uuid.New(),
+		CreatedAt: now,
+		UpdatedAt: now,
+		Name: name,
+	}
+
+	user, err := s.db.CreateUser(context.Background(), userParams)
+	if err != nil {
+		return fmt.Errorf("problem creating user: %v", err)
+	}
+
+	err = s.cfg.SetUser(name)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("user: %s was registered successfully!\ndata: %v\n", name, user)
 	return nil
 }
--- main.go
+++ main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"github.com/al3bdzo/Gator/internal/config"
+	"github.com/al3bdzo/Gator/internal/database"
+
 	"log"
 	"os"
+
+	"database/sql"
+	_ "github.com/lib/pq"
 )
 
 type state struct {
+	db *database.Queries
 	cfg *config.Config
 }
 
@@ -16,13 +22,23 @@ func main() {
 		log.Fatal(err)
 	}
 
+	dbURL := conf.DbURL
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	dbQueries := database.New(db)
+
 	programState := &state{
+		db: dbQueries,
 		cfg: &conf,
 	}
 	programCommands := commands{
 		cmds: make(map[string]func(*state, command)error),
 	}
 	programCommands.register("login", handlerLogin)
+	programCommands.register("register", handlerRegister)
 
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: cli <command> [args...]")
